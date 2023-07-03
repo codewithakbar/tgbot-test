20b2cb7cf9d4ff09e62c588eac568369a9b26b17
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-07-03 08:45:20 +0000

    add loop ever 10 sec

--- handlers/user_actions.py
+++ handlers/user_actions.py
@@ -1,5 +1,14 @@
+import datetime
+import asyncio
 from aiogram import types
-from dispatcher import dp
+from dispatcher import dp, bot
+from config import BOT_OWNERS
 
 
-# User actions in a group goes here ...
+async def send_message_every_10_sec():
+    await bot.send_message(BOT_OWNERS, 'H3110 FRI3ND')
+    while True:
+        await bot.send_message(BOT_OWNERS, datetime.datetime.now().strftime('%d.%m.%Y'))
+        await asyncio.sleep(10)
+
+        
