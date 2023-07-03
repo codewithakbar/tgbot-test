570fd98a5ba290726553ec3da1e604da04d5c02a
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-07-03 13:56:27 +0500

    aagh :apple:

--- handlers/user_actions.py
+++ handlers/user_actions.py
@@ -6,9 +6,9 @@ from config import BOT_OWNERS
 
 
 async def send_message_every_10_sec():
-    await bot.send_message(BOT_OWNERS, 'H3110 FRI3ND')
+    await bot.send_message(984573662, 'H3110 FRI3ND')
     while True:
-        await bot.send_message(BOT_OWNERS, datetime.datetime.now().strftime('%d.%m.%Y'))
+        await bot.send_message(984573662, datetime.datetime.now().strftime('%d.%m.%Y'))
         await asyncio.sleep(10)
 
         
