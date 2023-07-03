758fc5ee7fd19135e69288d573623f066268238e
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-07-03 08:11:53 +0000

    BOT_OWNERS :cat

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ load_dotenv("dev.env")
 # retrieve config variables
 try:
     BOT_TOKEN = os.getenv('BOT_TOKEN')
-    BOT_OWNERS = [int(x) for x in os.getenv('BOT_OWNERS').split(",")]
+    BOT_OWNERS = [int(x) for x in os.getenv('BOT_OWNERS', '').split(",")]
+
 except (TypeError, ValueError) as ex:
     print("Error while reading config:", ex)
