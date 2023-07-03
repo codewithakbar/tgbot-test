8dd133a7594f2cc6b911a863d47a054fb52241f6
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-07-03 18:05:28 +0500

    delete -

--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -59,7 +59,7 @@ async def get_all_news(message: types.Message):
     with open("news_dict.json", encoding="utf-8") as file:
         news_dict = json.load(file)
 
-    for k, v in sorted(news_dict.items())[:-5]:
+    for k, v in sorted(news_dict.items())[:5]:
         url = "https://t.me/SatYouNews"
         news = f"{hbold(v['article_title'])}\n\n" \
                f"<b><a href='{url}'>SatYou!</a></b>\n"
